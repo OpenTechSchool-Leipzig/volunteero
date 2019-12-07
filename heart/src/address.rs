8414a31a5acc24bde723188190cec70d58c572f8
Dominik Kellner <dkellner@dkellner.de>
2019-12-07 18:14:12 +0100

    Add `Opportunity.locations`

--- heart/src/address.rs
+++ heart/src/address.rs
@@ -1,2 +1,10 @@
-#[derive(Debug, PartialEq)]
-struct Address {}
+use serde::Serialize;
+
+#[derive(Debug, PartialEq, Clone, Serialize)]
+pub struct Address {
+    pub name: String,
+    pub street: String,
+    pub house_number: String,
+    pub postcode: String,
+    pub city: String,
+}
