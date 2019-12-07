f8486bcde0774a7bc104d1040600c10262dc62e9
Kristin Fritsch <ich@kristin-fritsch.de>
2019-12-07 14:57:21 +0100

    Add bot

---
+++ widgets/hello-volunteero/src/components/Bot.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>Hello Volunteer</div>
+    <LamaSvg />
+  </div>
+</template>
+
+<script>
+import LamaSvg from "@/components/svg/Lama.vue";
+
+export default {
+  components: { LamaSvg }
+};
+</script>
---
+++ widgets/hello-volunteero/src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Bot />
+  </div>
+</template>
+
+<script>
+import Bot from "@/components/Bot.vue";
+
+export default {
+  components: { Bot }
+};
+</script>
