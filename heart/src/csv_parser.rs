6ec5b8914da3c8fad8b6afef74375c69885d1a64
Dominik Kellner <dkellner@dkellner.de>
2019-12-11 17:56:26 +0100

    Move most of main.rs to lib.rs for integration tests
    Only library crates can leverage integration test functionality, so we aim to
    keep the "binary-only" part (in `main.rs`) as small and straightforward as
    possible.

--- heart/src/csv_parser.rs
+++ heart/src/csv_parser.rs
@@ -1,15 +1,13 @@
 use std::convert::TryInto;
-use std::env;
-use std::error::Error;
-use std::ffi::OsString;
 use std::fs::File;
+use std::io;
+use std::path::Path;
 
 use crate::dto::DTO;
 use crate::opportunity::Opportunity;
 
-pub fn csv_parser() -> Result<Vec<Opportunity>, Box<dyn Error>> {
-    let file_path = get_first_arg()?;
-    let file = File::open(file_path)?;
+pub fn csv_parser(path: &Path) -> Result<Vec<Opportunity>, io::Error> {
+    let file = File::open(path)?;
     let mut rdr = csv::Reader::from_reader(file);
     let mut opportunities: Vec<Opportunity> = Vec::new();
 
@@ -21,11 +19,4 @@ pub fn csv_parser() -> Result<Vec<Opportunity>, Box<dyn Error>> {
     println!("Successfully imported {} records.", opportunities.len());
 
     Ok(opportunities)
-}
-
-fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("expected 1 argument, but got none")),
-        Some(file_path) => Ok(file_path),
-    }
 }
---
+++ heart/src/lib.rs
@@ -0,0 +1,87 @@
+#![allow(dead_code)]
+#![feature(proc_macro_hygiene, decl_macro, type_ascription)]
+
+#[macro_use]
+extern crate rocket;
+extern crate reqwest;
+#[macro_use]
+extern crate lazy_static;
+
+mod address;
+mod contact;
+mod csv_parser;
+mod dto;
+mod geocoder;
+mod label;
+mod opportunity;
+mod organisation;
+mod repository;
+mod sample_data;
+
+use std::fmt;
+use std::path::Path;
+
+use rocket::response::NamedFile;
+use rocket::{Rocket, State};
+use rocket_contrib::json::Json;
+
+use crate::csv_parser::csv_parser;
+use crate::label::LabelList;
+use crate::opportunity::{Opportunity, OpportunityFilter, OpportunityRepository};
+use crate::repository::Repository;
+
+#[get("/opportunities")] // TODO: pagination
+fn index(opportunities: State<OpportunityRepository>) -> Json<Vec<Opportunity>> {
+    Json(opportunities.fetch_all())
+}
+
+/// GET /opportunities?labels=Sportart:Fußball,Kategorie:Vereinsarbeit,Kategorie:Vorstand
+#[get("/opportunities?<labels>")]
+fn find_by_labels(
+    labels: LabelList,
+    opportunities: State<OpportunityRepository>,
+) -> Json<Vec<Opportunity>> {
+    let label_filters: Vec<_> = labels
+        .0
+        .into_iter()
+        .map(OpportunityFilter::LabelFilter)
+        .collect();
+
+    Json(opportunities.find(&label_filters))
+}
+
+#[get("/swagger.yaml")]
+fn swagger_yaml() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/swagger.yaml")).ok()
+}
+
+// TODO: presumably our answers are missing proper encoding/chartes headers
+
+pub fn rocket(csv_path: &Path) -> Result<Rocket, UserFacingError> {
+    let opportunities = OpportunityRepository {
+        data: csv_parser(csv_path)?,
+    };
+
+    Ok(rocket::ignite()
+        .manage(opportunities)
+        .mount("/", routes![index, find_by_labels, swagger_yaml]))
+}
+
+#[derive(Debug, PartialEq)]
+pub struct UserFacingError {
+    pub message: String,
+}
+
+impl fmt::Display for UserFacingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl From<std::io::Error> for UserFacingError {
+    fn from(e: std::io::Error) -> Self {
+        UserFacingError {
+            message: format!("{}", e),
+        }
+    }
+}
